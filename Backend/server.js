77d6204179884f7cc475b2f5c662900b29eecebf
Noor9324098 <nooraddeenalhaddad2020@gmail.com>
2026-05-02 17:05:42 +0200

    Changed the deployment branch

--- Backend/server.js
+++ Backend/server.js
@@ -524,7 +524,7 @@ app.get("/api/aviationstack/search-flights", async (req, res) => {
     });
   }
 });
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

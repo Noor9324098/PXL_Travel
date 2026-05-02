fad167c17c1cb65bdb79b5884c148d1508fdba76
Noor9324098 <nooraddeenalhaddad2020@gmail.com>
2026-05-02 17:38:46 +0200

    Updated backend link to Render

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,4 +2,4 @@
 
 // require("dotenv").config({ path: path.resolve(__dirname, "src/.env") });
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
+  import.meta.env.VITE_API_BASE_URL || "https://pxl-travel.onrender.com";
